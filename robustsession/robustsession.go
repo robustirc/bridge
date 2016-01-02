4de3643a618120763d9ede743c5f33c41883a41a
Michael Stapelberg <michael@stapelberg.de>
2016-01-02 16:02:23 +0100

    bump version to 1.5

--- robustsession/robustsession.go
+++ robustsession/robustsession.go
@@ -31,7 +31,7 @@ const (
 	pathGetMessages   = "/robustirc/v1/%s/messages?lastseen=%s"
 )
 
-const Version = "RobustIRC Bridge v1.4"
+const Version = "RobustIRC Bridge v1.5"
 
 type robustId struct {
 	Id    int64
