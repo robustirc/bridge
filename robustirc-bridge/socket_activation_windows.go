116e8181425fa81d6d4a70491a00ee0404ae58a5
Jannis Andrija Schnitzer <jannis.schnitzer@infrontquant.com>
2021-01-27 12:44:33 +0100

    Fix build on Windows
    Windows has a different signature for the syscall.CloseOnExec func and
    based on quick research Windows's CloseOnExec might also behave
    differently. [1, 2]
    
    This commit moves the socket activation code into a
    conditionally-compiled file, with no-op implementations for Windows. The
    current implementation ignores the socket activation environment
    variables.
    
    [1] https://github.com/cloudflare/tableflip/issues/40
    [2] https://docs.microsoft.com/en-us/windows/win32/sysinfo/handles-and-objects

---
+++ robustirc-bridge/socket_activation_windows.go
@@ -0,0 +1,16 @@
+// +build windows
+
+package main
+
+import "errors"
+
+// nfds returns 0 to indicate that systemd socket activation is unsupported on Windows.
+func nfds() int {
+	return 0
+}
+
+// handleSocketActivation is no-op on Windows. It returns an error to be able terminate the program
+// instead of sleeping indefinitely.
+func handleSocketActivation() error {
+	return errors.New("systemd socket activation is not available on Windows")
+}
