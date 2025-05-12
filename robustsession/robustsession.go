aff85aece87407435f6dccfabd9f0af0edf2e75c
Michael Stapelberg <stapelberg@golang.org>
2025-05-12 16:25:00 +0200

    bump version to 1.11

--- robustsession/robustsession.go
+++ robustsession/robustsession.go
@@ -33,7 +33,7 @@ const (
 	pathGetMessages   = "/robustirc/v1/%s/messages?lastseen=%s"
 )
 
-const Version = "RobustIRC Bridge v1.10"
+const Version = "RobustIRC Bridge v1.11"
 
 type robustId struct {
 	Id    int64
